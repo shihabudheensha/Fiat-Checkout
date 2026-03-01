e3706d5afe809067c197e6f7b2eab3adc68b24c1
shihabudheensha <shihabudheen.sha.tk@gmail.com>
2026-03-01 22:30:24 +0530

    cardHolderNm validation done

---
+++ Scripts/validation.js
@@ -0,0 +1,41 @@
+﻿$(document).ready(function () {
+
+    function showError(input, message) {
+        $(input).addClass("error");
+        $(input).closest(".form-group").find(".error-message").text(message);
+    }
+
+    function clearError(input) {
+        $(input).removeClass("error");
+        $(input).closest(".form-group").find(".error-message").text("");
+    }
+    function validateCardholder() {
+        let value = $("#cardholderName").val().trim();
+        let regex = /^[A-Za-z\s'-]+$/;
+
+
+        if (value === "") {
+            showError("#cardholderName", "Cardholder name is required");
+            return false;
+        }
+
+        if (value.length < 3) {
+            showError("#cardholderName", "Minimum 3 characters required");
+            return false;
+        }
+
+        if (!regex.test(value)) {
+            showError("#cardholderName", "Only letters allowed");
+            return false;
+        }
+
+        clearError("#cardholderName");
+        return true;
+    }
+
+
+    $("#cardholderName").on("blur", validateCardholder);
+    $("#cardholderName").on("input", validateCardholder);
+
+
+});
